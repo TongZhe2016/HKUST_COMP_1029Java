578ea02621fa80d51394a01316c32acf16bcdf53
William Tong <31505695+TongZhe2016@users.noreply.github.com>
2023-02-22 10:33:48 +0800

    Add files via upload

---
+++ L1.1_Hello World/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * This is the first program in the Java bridging course.
+ */
+public class HelloWorld
+{
+    /**
+     * The main method of the program.
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hey, how are you doing?");
+    }
+}
